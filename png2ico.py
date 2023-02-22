99e7f008a0382305343a83fe2eb4e068f009eab1
j-finger <jacob.finger@my.jcu.edu.au>
2023-02-22 19:46:52 +1000

    Revert back to old code, new code was incomplete

--- png2ico.py
+++ png2ico.py
@@ -1,29 +1,8 @@
 import sys
 import os
-from PyQt5.QtWidgets import QMainWindow, QApplication, QLabel, QVBoxLayout, QWidget, QProgressBar
+from PyQt5.QtWidgets import QApplication, QMainWindow, QFileDialog, QLabel
 from PyQt5.QtGui import QPixmap
-from PyQt5.QtCore import Qt, QThread, pyqtSignal
-
-
-class ConverterThread(QThread):
-    progress_update = pyqtSignal(int)
-    finished_conversion = pyqtSignal(str)
-
-    def __init__(self, file_path, output_path):
-        super().__init__()
-        self.file_path = file_path
-        self.output_path = output_path
-
-    def run(self):
-        # Convert the PNG file to an ICO file using the command line tool "magick"
-        os.system("magick convert {} {}".format(self.file_path, self.output_path))
-
-        # Signal the completion of the conversion process
-        self.finished_conversion.emit(self.output_path)
-
-    def update_progress(self, value):
-        self.progress_update.emit(value)
-
+from PyQt5.QtCore import Qt
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -38,20 +17,11 @@ class MainWindow(QMainWindow):
         self.label = QLabel("Drag and drop a PNG file here")
         self.label.setAlignment(Qt.AlignCenter)
         self.label.setWordWrap(True)
-        self.label.setStyleSheet("QLabel { background-color: #444444; border: 1px solid gray; padding: 10px; color: "
-                                 "white; }")  # set the label's background color to light gray and its text color to
-        # white
+        self.label.setStyleSheet("QLabel { background-color: #444444; border: 1px solid gray; padding: 10px; color: white; }")  # set the label's background color to light gray and its text color to white
 
-        # Create a progress bar
-        self.progress_bar = QProgressBar()
-        self.progress_bar.setTextVisible(False)
-        self.progress_bar.setStyleSheet("QProgressBar { background-color: #444444; border: 1px solid gray; padding: 1px; }"
-                                         "QProgressBar::chunk { background-color: #77aaff; }")
-
-        # Create a layout to hold the label and progress bar
+        # Create a layout to hold the label
         layout = QVBoxLayout()
         layout.addWidget(self.label)
-        layout.addWidget(self.progress_bar)
 
         # Set the layout as the central widget of the main window
         central_widget = QWidget()
@@ -69,19 +39,18 @@ class MainWindow(QMainWindow):
         # Get the file path of the dropped file
         file_path = event.mimeData().urls()[0].toLocalFile()
 
-        # Create the output path
+        # Convert the PNG file to an ICO file using the command line tool "magick"
         output_path = os.path.join(os.path.dirname(file_path), os.path.splitext(os.path.basename(file_path))[0] + ".ico")
+        os.system("magick convert {} {}".format(file_path, output_path))
 
-        # Create a converter thread to convert the file
-        converter_thread = ConverterThread(file_path, output_path)
-        converter_thread.finished_conversion.connect(self.conversion_finished)
-        converter_thread.progress_update.connect(self.update_progress_bar)
-        converter_thread.start()
-
-        # Update the label to show that the conversion is in progress
-        self.label.setText("Converting PNG file to ICO file...\n\nThis may take some time.")
-
-    def conversion_finished(self, output_path):
         # Update the label to show the conversion was successful
         pixmap = QPixmap(output_path)
-        self.label
+        self.label.setPixmap(pixmap)
+        self.label.resize(pixmap.width(), pixmap.height())
+        self.label.setText("PNG file converted to ICO file:\n{}".format(output_path))
+
+if __name__ == "__main__":
+    app = QApplication(sys.argv)
+    window = MainWindow()
+    window.show()
+    sys.exit(app.exec_())
